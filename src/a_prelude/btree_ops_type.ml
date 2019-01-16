4d5779c6e0a94371af7874ceebb9a23e512fd20b
Tom Ridge <tom.j.ridge@googlemail.com>
2019-01-16 15:40:21 +0000

    +

--- src/a_prelude/btree_ops_type.ml
+++ src/a_prelude/btree_ops_type.ml
@@ -1,9 +1,13 @@
 open Tjr_monad.Types
 
 (* FIXME also include "batch" op *)
+
+(** The operations supported by the B-tree. Even though the B-tree is
+   synchronous, the "sync" operation is used to reveal the current
+   root block. *)
 type ('k,'v,'blkid,'t) btree_ops = {
   find: 'k -> ('v option,'t)m;
   insert: 'k -> 'v -> (unit,'t)m;
   delete: 'k -> (unit,'t)m;
-  sync: unit -> ('blkid,'t)m;
+  sync: unit -> ('blkid,'t)m;  
 }
---
+++ src/a_prelude/syncable_map_type.ml
@@ -0,0 +1,19 @@
+open Tjr_monad.Types
+
+(** A syncable map API.
+
+Sync operations:
+
+- sync to sync the whole map to disk
+- ksync to sync a particular key
+- ks_sync to sync a set of keys
+
+*)
+type ('k,'v,'ptr,'t) syncable_map = {
+  find: 'k -> ('v option,'t)m;
+  insert: 'k -> 'v -> (unit,'t)m;
+  delete: 'k -> (unit,'t)m;
+  sync: unit -> ('ptr,'t)m;
+  ksync: 'k -> ('ptr,'t)m;
+  ks_sync: 'k list -> ('ptr,'t)m
+}
